d9b9af67ebe66c562cab6aa63b27a90b582dedf6
johnfawkes <skaterdudebradley@gmail.com>
2018-07-17 12:05:46 -0400

    -fix typos -new preset -remove not needed .aml.sh -add a no config option -comment music_helper under osp -fix configs/presets "stacking" in module.prop when changing to different config/preset
    signed off by JohnFawkes

--- common/install.sh
+++ common/install.sh
@@ -67,20 +67,9 @@ if $MAGISK && ! $SYSOVERRIDE; then
   fi
 fi
 
-# GET OLD/NEW FROM ZIP NAME
-#CONF1=false; CONF2=false; CONF3=false; CONF4=false; CONF0=false;
-#case $(basename $ZIP) in
-#  *conf1*|*Conf1*|*CONF1*) CONF1=true;;
-#  *conf2*|*Conf2*|*CONF2*) CONF2=true;;
-#  *conf3*|*Conf3*|*CONF3*) CONF3=true;;
-#  *conf4*|*Conf4*|*CONF4*) CONF4=true;;
-#  *none*|*None*|*NONE*) CONF0=true;;
-#esac
-
 # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
 KEYCHECK=$INSTALLER/common/keycheck
 chmod 755 $KEYCHECK
-
 
 ui_print " "
 ui_print "   Removing remnants from past Ice installs..."
@@ -111,7 +100,6 @@ for REMNANT in $(find /data -name "*IceSoundService*" -o -name "*AudioWizard*" -
 done
 
 ui_print ""
-#if ! $CONF1 && ! $CONF2 && ! $CONF3 && ! $CONF4 && ! $CONF0; then
  if keytest; then
   FUNCTION=chooseconfig
  else
@@ -136,39 +124,44 @@ ui_print ""
   ui_print " "
   ui_print "   Vol+ = PureICE  Vol- = IceWizard"
   if $FUNCTION; then
-    PURE=true
+  PURE=true
   else
-    ICEW=true
+  ICEW=true
   fi
  fi
 
-  ui_priant " "
+  ui_print " "
   ui_print "   Choose which config you want installed:"
   ui_print "   Processing may either occur or not occur"
   ui_print "   depending which config is used."
+  ui_print " "
+  ui_print "   Vol+ = Config 1  Vol- = Config 2, 3 or 4"
+ if $FUNCTION; then
+ CONF1=true
+ else
+  ui_print " "
+  ui_print "   Choose which config you want installed:"
+  ui_print "   Vol+ = Config2 Vol- = Config 3 or 4"
   if $FUNCTION; then
-  conf2=true
+  CONF2=true
   else
-    ui_print " "
-    ui_print "   Choose which config you want installed:"
-    ui_print "   Vol+ = Config 3 Vol- = Config or No Config"
+  ui_print " "
+  ui_print "   Choose which config you want installed:"
+  ui_print "   Vol+ = Config 3 Vol- = Config 4 or No Config"
    if $FUNCTION; then
-     CONF3=true
+   CONF3=true
    else
-    ui_print " "
-    ui_print "   Choose which config you want installed:"
-    ui_print "   Vol+ = Config 4 Vol- = No Config"
+  ui_print " "
+  ui_print "   Choose which config you want installed:"
+  ui_print "   Vol+ = Config 4 Vol- = No Config"
     if $FUNCTION; then
-      conf4=true
+    CONF4=true
     else
-      conf0=true
-    fi
+    CONF0=true
+    fi 
    fi
   fi
-# fi  
-#else
-#  ui_print "   Config specified in zipname!"
-#fi
+ fi
 
 # Prep terminal script
 sed -i -e "s|<MAGISK>|$MAGISK|" -e "s|<PROP>|$PROP|" -e "s|<MODPROP>|$MOD_VER|" -e "s|<ROOT>|$ROOT|" $INSTALLER/system/xbin/icewizard
@@ -266,7 +259,7 @@ fi
 
  ui_print ""
  ui_print " Installing Custom Presets (Thanks Arise Team!!!)"
- ui_print " Please Run icewizard in Terminal to Change Presets/Configs/Props"
+ ui_print " Please Run su icewizard in Terminal to Change Presets/Configs/Props"
  ui_print " No Need To Reboot After Changing. IceSound will be Killed to Apply Preset/Config"
  ui_print " A Reboot is required to Apply Prop"
  cp -rf $INSTALLER/custom/IceWizard $SDCARD
@@ -286,6 +279,7 @@ for OFILE in ${CFGS}; do
             sed -i "/icepower_null {/,/}/d" $FILE
             sed -i "/icepower_load {/,/}/d" $FILE
             sed -i "/icepower_test {/,/}/d" $FILE
+            sed -i "/^output_session_processing {/,/^}/ {/music_helper {/,/}/ s/^/#$MODID/g}" $FILE
             sed -i "s/^libraries {/libraries {\n  icepower { #$MODID\n    path $LIBPATCH\/lib\/soundfx\/libicepower.so\n  } #$MODID/g" $FILE
             sed -i "s/^effects {/effects {\n  icepower_test { #$MODID\n    library icepower\n    uuid e5456320-5391-11e3-8f96-0800200c9a66\n  } #$MODID/g" $FILE
             sed -i "s/^effects {/effects {\n  icepower_load { #$MODID\n    library icepower\n    uuid bf51a790-512b-11e3-8f96-0800200c9a66\n  } #$MODID/g" $FILE
@@ -293,12 +287,18 @@ for OFILE in ${CFGS}; do
 #            sed -i "s/^effects {/effects {\n  icepower_eq { #$MODID\n    library icepower\n    uuid 50dbef80-4ad4-11e3-8f96-0800200c9a66\n  } #$MODID/g" $FILE
             sed -i "s/^effects {/effects {\n  icepower_algo { #$MODID\n    library icepower\n    uuid f1c02420-777f-11e3-981f-0800200c9a66\n  } #$MODID/g" $FILE;;
     *.xml) sed -i "/icepower/d" $FILE
-           sed -i "/<libraries>/ a\        <library name=\"icepower\" path=\"libicepower.so\"\/><!--$MODID-->" $FILE
-           sed -i "/<effects>/ a\        <effect name=\"icepower_test\" library=\"icepower\" uuid=\"e5456320-5391-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
-           sed -i "/<effects>/ a\        <effect name=\"icepower_load\" library=\"icepower\" uuid=\"bf51a790-512b-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
-           sed -i "/<effects>/ a\        <effect name=\"icepower_null\" library=\"icepower\" uuid=\"63509430-52aa-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
-#           sed -i "/<effects>/ a\        <effect name=\"icepower_eq\" library=\"icepower\" uuid=\"50dbef80-4ad4-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
-           sed -i "/<effects>/ a\        <effect name=\"icepower_algo\" library=\"icepower\" uuid=\"f1c02420-777f-11e3-981f-0800200c9a66\"\/><!--$MODID-->" $FILE;;
+            sed -i "/icepower_algo {/,/}/d" $FILE
+#            sed -i "/icepower_eq {/,/}/d" $FILE
+            sed -i "/icepower_null {/,/}/d" $FILE
+            sed -i "/icepower_load {/,/}/d" $FILE
+            sed -i "/icepower_test {/,/}/d" $FILE
+            sed -i "s/^            <apply effect=\"music_helper\"\/>/            <!--apply effect=\"music_helper\"\/-->/g" $FILE
+            sed -i "/<libraries>/ a\        <library name=\"icepower\" path=\"libicepower.so\"\/><!--$MODID-->" $FILE
+            sed -i "/<effects>/ a\        <effect name=\"icepower_test\" library=\"icepower\" uuid=\"e5456320-5391-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
+            sed -i "/<effects>/ a\        <effect name=\"icepower_load\" library=\"icepower\" uuid=\"bf51a790-512b-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
+            sed -i "/<effects>/ a\        <effect name=\"icepower_null\" library=\"icepower\" uuid=\"63509430-52aa-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
+#            sed -i "/<effects>/ a\        <effect name=\"icepower_eq\" library=\"icepower\" uuid=\"50dbef80-4ad4-11e3-8f96-0800200c9a66\"\/><!--$MODID-->" $FILE
+            sed -i "/<effects>/ a\        <effect name=\"icepower_algo\" library=\"icepower\" uuid=\"f1c02420-777f-11e3-981f-0800200c9a66\"\/><!--$MODID-->" $FILE;;
     esac
 done
 
